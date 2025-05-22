ad2ef12e4767a5da952607c984e22e4f25796e03
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-22 17:23:11 -0300

    Fix typo in MongoDB connection log message

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {});
-        console.log("MnongoDB conected");
+        console.log("MongoDB conected");
         
     } catch (err) {
         console.error("Error connecting to MongoDB". err);
