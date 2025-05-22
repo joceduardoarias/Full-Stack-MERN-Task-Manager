0d456085cf2f1a103af824764f17e450ec9d1b51
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-22 17:19:54 -0300

    Feat: Inicializar conexión a la base de datos MongoDB
    Configura la conexión a la base de datos MongoDB utilizando Mongoose.
    Esto permite la interacción con la base de datos para las operaciones CRUD.
    Se comenta el uso de rutas hasta que se definan los modelos.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const { Console } = require("console");
+
+const mongoose = require('./config/db.js');
+const { connect } = require("http2");
 
 const app = express();
 
@@ -18,8 +20,15 @@ app.use(
 // Middleware
 app.use(express.json());
 
+// Connect DataBase
+mongoose();
+
 // Routes
+// app.use("api/auth", authRoutes);
+// app.use("api/report", reportRoutes);
+// app.use("api/task", taskhRoutes);
+// app.use("api/user", userRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => Console.log(`Servidor corriendo en http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
