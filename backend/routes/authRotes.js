e54d19600a7b8ca6b627be249b49ef7634411145
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-23 14:42:40 -0300

    Updates user authentication routes.
    Changes the authentication routes to use GET for profile retrieval instead of POST.
    
    Corrects the updateUserProfile route definition.

--- backend/routes/authRotes.js
+++ backend/routes/authRotes.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const { registerUser, loginUser, getUserProfile, updateUserProfile} = require("../controllers/authController")
 
 const router = express.Router();
 
 // Auth Routes
 router.post("/register", registerUser); 
 router.post("/register", loginUser); 
-router.post("/profile", protect, getUserProfile); 
-router.put("/profile", protect, updateuserProfile);
+router.get("/profile", protect, getUserProfile); 
+router.put("/profile", protect, updateUserProfile);
 
 module.exports = router;
