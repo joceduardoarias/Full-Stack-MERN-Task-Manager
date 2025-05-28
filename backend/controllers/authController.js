ed6320c470f918cc4c3a117045a03498389e59ab
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-28 15:02:38 -0300

    Updates user profile information.
    Implements functionality to update user profile details, including name, email, and password.
    It also handles password hashing when a new password is provided and returns user data with JWT upon successful update.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -106,7 +106,32 @@ const getUserProfile = async (req, res) => {
 // @acces Public
 const updateUserProfile = async (req, res) => {
     try {
+        const user = await User.findById(req.user.id);
 
+        if (!user) {
+            return res.status(404).json({ message: "User not Found" });
+        }
+
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(req.body.password, salt);
+        }
+
+        const updatedUser = await user.save();
+
+          // Return user data with JWT
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            role: updatedUser.role,
+            profileImageUrl: updatedUser.profileImageUrl,
+            token: generateToke(updatedUser._id)
+        });
+        
     } catch (err) {
         res.status(500).json({ message: "Server error", error: err.message })
     }
