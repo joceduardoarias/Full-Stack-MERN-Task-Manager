5bb1ee6fa21250bbbb0749dc39901d3a55fee9c8
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-28 11:26:29 -0300

    Corrige: Asigna la ruta correcta para login
    Corrige la ruta para el inicio de sesión de usuarios,
    asignando la ruta POST correcta para el endpoint de login.
    También, agrega middleware de protección a las rutas de perfil
    y actualización de perfil.

--- backend/routes/authRotes.js
+++ backend/routes/authRotes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { registerUser, loginUser, getUserProfile, updateUserProfile} = require("../controllers/authController")
-
+const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 // Auth Routes
 router.post("/register", registerUser); 
-router.post("/register", loginUser); 
+router.post("/login", loginUser); 
 router.get("/profile", protect, getUserProfile); 
 router.put("/profile", protect, updateUserProfile);
 
