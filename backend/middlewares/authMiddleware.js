8dcd726b84f07cc82cb4dd824697a5a27429674f
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-28 11:25:48 -0300

    Implementa autenticación y autorización
    Añade middleware para proteger rutas con JWT.
    Implementa autorización basada en roles (admin).
    Maneja errores de autenticación y autorización.

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+// Middelware to protect routes
+const protect = async (req, res, next) => {
+    try {
+        let token = req.headers.authorization;
+
+        if (token && token.startsWith("Bearer")) {
+            token = token.split(" ")[1]; //Extrac token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.id).select("-password");
+            next();
+        } else {
+            res.status(401).json({ message: "Not authorized, no token" });
+        }
+    } catch (err) {
+        res.status(401).json({ message: "Token failed", error: err.message });
+    }
+}
+
+// Middleware for Admin-only acces
+
+const adminOnly = (req, res, next) => {
+    if (req.user && req.user.roll === "admin") {
+        next();
+    } else {
+        res.status(403).json = ({ message: " Access denied, admin only" });
+    }
+}
+
+module.exports = { protect, adminOnly };
