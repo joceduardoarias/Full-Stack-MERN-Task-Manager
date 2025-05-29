5ad21ef9593f6cceb8e1311f891fdfa7de1c79f6
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-29 12:01:45 -0300

    Crea rutas para la gestión de tareas.
    Define las rutas para las operaciones CRUD de las tareas,
    incluyendo la obtención de datos para el dashboard, la
    gestión de estado y checklist, e implementa middlewares
    de autenticación y autorización.

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { protect, adminOnly } = require("../middlewares/authMiddleware");
+const {
+    getTask,
+    getTaskById,
+    createTask,
+    updateTask,
+    deleteTask,
+    updateTaskStatus,
+    updateTaskCheckList,
+    getDashboardData,
+    getUserDashboardData
+} = require("../controllers/taskController");
+
+const router = express.Router();
+
+// Task Management Routes
+router.get("/dashboar-data", protect, getDashboardData);
+router.get("/user-dashboar-data", protect, getUserDashboardData);
+router.get("/", protect, getTask);
+router.get("/:id", protect, getTaskById);
+router.post("/", protect, adminOnly, createTask);
+router.put("/:id", protect, updateTask);
+router.delete("/:id", protect, adminOnly, deleteTask);
+router.get("/:id/status", protect, updateTaskStatus);
+router.get("/:id/todo", protect, updateTaskCheckList);
+
+module.exports = router;
