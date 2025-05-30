1adbb1855763949b86a3281a68c455a858623cf4
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-30 15:25:48 -0300

    Feat: Agrega rutas para exportar reportes
    Implementa las rutas necesarias para la generación y exportación de reportes de tareas y usuarios en formato Excel/PDF.
    
    Las rutas están protegidas y accesibles solo para administradores.

---
+++ backend/routes/reportRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { protect, adminOnly } = require("../middlewares/authMiddleware");
+const { exportTaskReport, exportUserReport } = require("../controllers/reportController")
+
+const router = express.Router();
+
+router.get("/export/tasks", protect, adminOnly, exportTaskReport); // Export all tasks as Excel/PDF
+router.get("/export/tasks", protect, adminOnly, exportUserReport); // Export user-task report
+
+module.exports = router;
