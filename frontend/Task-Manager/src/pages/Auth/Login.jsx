697749bfcf79e234b04718df4ea4b06692ff50e7
Jose Arias <josee.ariasmelo@gmail.com>
2025-06-06 15:10:51 -0300

    Implementa página de inicio de sesión.
    Añade la estructura y los componentes básicos para la página de inicio de sesión.
    Permite a los usuarios introducir sus credenciales (correo electrónico y contraseña).
    Incluye enlace a la página de registro.

--- frontend/Task-Manager/src/pages/Auth/Login.jsx
+++ frontend/Task-Manager/src/pages/Auth/Login.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from "react"
+import AuthLayout from "../../components/layouts/AuthLayout"
+import { Link, useNavigate } from "react-router-dom"
+import Input from "../../components/Inputs/Input"
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+  const navigate = useNavigate();
 
-export default Login
+  const handleLogin = async (e) => {
+    e.preventDefault()
+  };
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcom Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">Please enter your details to log in</p>
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="john@example.com"
+            type="text"
+          />
+          <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Min 8 characters"
+            type="password"
+          />
+        </form>
+        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+        <button type="submit" className="btn-primary">
+          LOGIN
+        </button>
+        <p className="text-[13px] text-slate-800 mt-3">
+          Don't have an account?{" "}
+
+          <Link className="font-medium text-primary underline" to="/singUp" >
+            SingUp
+          </Link>
+        </p>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default Login;
